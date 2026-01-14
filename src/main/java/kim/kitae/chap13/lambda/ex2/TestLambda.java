8e69724641f076b372094cd86a37a23f4bc6f35c
Kitae's Github <aqua0405@gmail.com>
2026-01-14 14:54:42 +0900

    Create TestLambda.java

---
+++ src/main/java/kim/kitae/chap13/lambda/ex2/TestLambda.java
@@ -0,0 +1,20 @@
+package kim.kitae.chap13.lambda.ex2;
+
+public class TestLambda {
+    public static void main(String[] args) {
+        PrintString printString = str -> System.out.println(str);
+        printString.showString("Hello, Lambda_1");
+        showMyString(printString);
+
+        PrintString returnStr = returnString();
+        returnStr.showString("Hello");
+    }
+
+    public static void showMyString(PrintString p) {
+        p.showString("Hello, Lambda_2");
+	}
+
+    public static PrintString returnString() {
+        return str -> System.out.println(str + " world");
+    }
+}
