c255b501ff490e69a6c6b42e31479e13f74ad6f6
Kitae's Github <aqua0405@gmail.com>
2026-01-22 10:21:39 +0900

    스레드

---
+++ src/main/java/kim/kitae/chap16/ex2/MyRunnable.java
@@ -0,0 +1,11 @@
+package kim.kitae.chap16.ex2;
+
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        int i;
+        for(i=0; i<=200; i++) {
+            System.out.println(Thread.currentThread().getName() + ":" + i );
+        }
+    }
+}
