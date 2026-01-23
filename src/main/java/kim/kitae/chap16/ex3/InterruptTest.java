e605dad9d0db9f938b5478c8af5f88fbab65d42e
Kitae's Github <aqua0405@gmail.com>
2026-01-24 08:44:04 +0900

    스레드 처리

---
+++ src/main/java/kim/kitae/chap16/ex3/InterruptTest.java
@@ -0,0 +1,24 @@
+package kim.kitae.chap16.ex3;
+
+public class InterruptTest extends Thread {
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread.sleep(10);
+                System.out.print(i + "\t");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("인터럽트 발생");
+        }
+        System.out.println("end");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        InterruptTest interruptTest = new InterruptTest();
+        interruptTest.start();
+        Thread.sleep(50);
+        interruptTest.interrupt();
+
+    }
+}
