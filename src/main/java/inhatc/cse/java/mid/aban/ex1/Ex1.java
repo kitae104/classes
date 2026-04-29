6beaf2b7c5ec36a645a9fafac11e5e84880dceac
Kitae's Github <aqua0405@gmail.com>
2026-04-29 13:28:08 +0900

    Update Ex1.java

--- src/main/java/inhatc/cse/java/mid/aban/ex1/Ex1.java
+++ src/main/java/inhatc/cse/java/mid/aban/ex1/Ex1.java
@@ -1,6 +1,9 @@
 package inhatc.cse.java.mid.aban.ex1;
+
+import java.util.List;
+
 /*
-umNumbers(List<? extends Number>) 메서드를 작성해 숫자 리스트 합계를 double로 반환하세요.
+sumNumbers(List<? extends Number>) 메서드를 작성해 숫자 리스트 합계를 double로 반환하세요.
 
 테스트
 [1, 2, 3, 4]
@@ -8,6 +11,16 @@ umNumbers(List<? extends Number>) 메서드를 작성해 숫자 리스트 합계
  */
 public class Ex1 {
     public static void main(String[] args) {
-
+        System.out.println(sumNumbers(List.of(1, 2, 3, 4)));
+        System.out.println(sumNumbers(List.of(1.5, 2.0, 3.5)));
     }
+
+    private static double sumNumbers(List<? extends Number> numbers) {
+        double sum = numbers.stream()
+                .mapToDouble(x -> x.doubleValue())
+                .sum();
+        return sum;
+    }
+
+
 }
