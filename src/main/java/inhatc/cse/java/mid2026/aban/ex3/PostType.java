5d3b8f6833a5d542dd5131077209998acd5900ac
Kitae's Github <aqua0405@gmail.com>
2026-06-12 15:31:48 +0900

    중간고사 삭제

--- src/main/java/inhatc/cse/java/mid2026/aban/ex3/PostType.java
+++
@@ -1,5 +0,0 @@
-package inhatc.cse.java.mid2026.aban.ex3;
-
-public enum PostType {
-    NOTICE, QNA, FREE
-}
--- src/main/java/inhatc/cse/java/mid2026/cban/ex1/Book.java
+++
@@ -1,4 +0,0 @@
-package inhatc.cse.java.mid2026.cban.ex1;
-
-public record Book(String bookNo, String title, int price) {
-}
